6eb4b735e520402d215a82c6ff4c3bee3212d31b
Daniel Sasse <dsasse07@gmail.com>
2021-04-19 23:55:41 -0400

    base structure added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header'
+import ArticleCarousel from './components/ArticleCarousel/ArticleCarousel';
+import OrgInfo from './components/OrgInfo/OrgInfo';
+import Successes from './components/Successes/Successes';
+import Organizations from './components/Organizations/Organizations'
+import Footer from './components/Footer/Footer'
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <Header />
+      <main>
+        <ArticleCarousel />
+        <OrgInfo />
+        <Successes />
+        <Organizations />
+      </main>
+      <Footer />
+    </>
   );
+
 }
 
 export default App;
