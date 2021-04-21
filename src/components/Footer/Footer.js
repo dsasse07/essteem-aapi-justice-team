97d42a0a0661bc79256a4190eb73188ebe0fef3f
Daniel Sasse <dsasse07@gmail.com>
2021-04-21 11:31:56 -0400

    Footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,53 @@
 import './Footer.css'
-function Footer() {
+import footerLogo from '../../assets/images/footer-logo.png'
+import SocialLink from './SocialLink'
+
+const Footer = () => {
   return (
     <footer className="footer-container row">
-      Footer
+
+      <div className="footer-logo-container">
+        <img src={footerLogo} alt="AAJC logo" />
+      </div>
+
+      <a 
+        href="https://secure.donationpay.org/aajc/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        DONATE TODAY!
+      </a>
+
+      <div className="social-container">
+        <SocialLink 
+          title="Twitter"
+          url="https://twitter.com/AAAJ_AAJC"
+        />
+        <SocialLink 
+          title="Facebook"
+          url="https://www.facebook.com/advancingjusticeaajc"
+        />
+        <SocialLink 
+          title="Instagram"
+          url="https://www.instagram.com/advancingjustice_aajc/"
+        />
+      </div>
+
+      <div className="disclaimer-container">
+        <p>
+          Asian Americans Advancing Justice
+        </p>
+        <p>
+          Some rights reserved.
+        </p>
+        <p>
+          All images used are licensed under the Creative Commons License.
+        </p>
+        <p>
+          Please contact your local affiliate to learn more about our work.
+        </p>
+      </div>
+
     </footer>
   )
 }
